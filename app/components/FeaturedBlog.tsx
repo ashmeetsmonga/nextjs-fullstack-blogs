fcf0ef904db516a2eda83d8de5031deb4083bf8b
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-02 23:44:11 +0530

    ui changes

--- app/components/FeaturedBlog.tsx
+++ app/components/FeaturedBlog.tsx
@@ -6,13 +6,13 @@ const FeaturedBlog = () => {
     <div className="relative h-[30rem] w-full">
       <div className="absolute left-0 top-0 h-full w-full">
         <Image
-          src={"/images/hero-image.jpg"}
+          src="/images/hero-image-1.jpg"
           alt="featured-blog-image"
           fill
           objectFit="cover"
         />
       </div>
-      <div className="relative z-10 ml-5 flex h-4/5 w-1/2 flex-col justify-center gap-4 bg-white p-10 md:w-1/3">
+      <div className="relative z-10 ml-5 flex h-4/5 w-1/2 flex-col justify-center gap-4 bg-white p-10 md:w-1/3 lg:ml-10">
         <div>
           <div className="text-xs font-light uppercase text-gray-400 lg:text-lg">
             Featured Blog
