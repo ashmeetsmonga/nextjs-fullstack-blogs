c9ea93d06b7136ec7d4410b8040632b76a1bcf6c
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-03 00:00:34 +0530

    navbar ui corrections

---
+++ app/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="rounded bg-black px-4 py-2 font-bold uppercase text-white">
+      Blogs
+    </div>
+  );
+};
+
+export default Logo;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,15 @@
 import React from "react";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return (
     <div className="flex w-full justify-between p-5">
       <div className="flex items-center gap-8">
-        <div className="rounded bg-black px-4 py-2 font-bold uppercase text-white">
-          Blogs
-        </div>
+        <Logo />
         <div>Home</div>
         <div>About</div>
       </div>
-      <div className="flex gap-8">
+      <div className="flex items-center gap-8">
         <div>Login</div>
         <div>Register</div>
       </div>
