6a4567d436894d86dea1dcfa9d44c3418f5586f6
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-03 22:04:47 +0530

    states added and api connected in register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onRegister = () => {
+    axios.post("/api/register", { name, email, password });
+  };
+
   return (
     <div className="flex w-full justify-center">
       <div className="flex w-full max-w-[700px] flex-col items-center gap-1 lg:gap-4">
@@ -15,23 +26,34 @@ const RegisterPage = () => {
             className="w-full rounded-sm bg-gray-100 p-4 text-sm outline-none focus:outline-none lg:text-base"
             type="text"
             placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <input
             className="w-full rounded-sm bg-gray-100 p-4 text-sm outline-none focus:outline-none lg:text-base"
             type="text"
             placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             className="w-full rounded-sm bg-gray-100 p-4 text-sm outline-none focus:outline-none lg:text-base"
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <input
             className="w-full rounded-sm bg-gray-100 p-4 text-sm outline-none focus:outline-none lg:text-base"
             type="password"
             placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
-          <button className="mt-4 w-fit rounded-sm bg-black px-4 py-2 uppercase text-white">
+          <button
+            onClick={onRegister}
+            className="mt-4 w-fit rounded-sm bg-black px-4 py-2 uppercase text-white"
+          >
             Register
           </button>
         </div>
