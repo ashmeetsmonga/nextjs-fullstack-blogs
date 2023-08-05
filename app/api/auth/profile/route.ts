4d816939501a4a65ed55a57eaf8707918ab40cff
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-05 20:41:45 +0530

    added profile route

---
+++ app/api/auth/profile/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/libs/prisma";
+
+export async function GET(req: NextRequest) {
+  const userID = req.headers.get("userID");
+
+  const user = await prisma.user.findUnique({ where: { id: userID! } });
+  return NextResponse.json({ name: user!.name, email: user!.email });
+}
--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -27,7 +27,7 @@ export async function POST(req: Request) {
     const user = await prisma.user.create({
       data: { name, username, email, hashedPassword },
     });
-    const response = NextResponse.json(user);
+    const response = NextResponse.json({ name: user.name, email: user.email });
 
     const token = await createToken({ id: user.id, email: user.email! });
     response.cookies.set("token", token);
