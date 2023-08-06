cc2a9f9cd44278d6f40646294f6a79580066b462
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-06 15:51:56 +0530

    ui changes

--- app/components/FeaturedBlog.tsx
+++ app/components/FeaturedBlog.tsx
@@ -3,25 +3,26 @@ import React from "react";
 
 const FeaturedBlog = () => {
   return (
-    <div className="relative h-[30rem] w-full">
+    <div className="relative h-[30rem] w-full px-5">
       <div className="absolute left-0 top-0 h-full w-full">
         <Image
           src="/images/hero-image-1.jpg"
           alt="featured-blog-image"
           fill
           objectFit="cover"
+          className=""
         />
       </div>
-      <div className="relative z-10 ml-5 flex h-4/5 w-1/2 flex-col justify-center gap-4 bg-white p-10 lg:ml-10 lg:w-1/3">
+      <div className="relative z-10 flex h-1/2 flex-col justify-center gap-4 bg-white p-10 lg:ml-10 lg:h-4/5 lg:w-1/3">
         <div>
           <div className="text-xs font-light uppercase text-gray-400 lg:text-lg">
             Featured Blog
           </div>
-          <div className="font-semibold capitalize lg:text-4xl">
+          <div className="text-2xl font-semibold capitalize lg:text-4xl">
             World's most dangerous technology ever made
           </div>
         </div>
-        <div className="flex w-full justify-between text-xs font-light text-gray-400 lg:text-sm">
+        <div className="flex gap-4 text-xs font-light text-gray-400 lg:text-sm">
           <p>Ralph Lauren</p>
           <p>01 Aug 2023</p>
         </div>
