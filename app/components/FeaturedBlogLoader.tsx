3b3b15bf8803d094217b665c8a4145844b028d00
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-07 21:44:58 +0530

    added featured blog loader

---
+++ app/components/FeaturedBlogLoader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FeaturedBlogLoader = async () => {
+  return (
+    <div className="relative h-[30rem] w-full animate-pulse bg-gray-400 px-5"></div>
+  );
+};
+
+export default FeaturedBlogLoader;
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import { getBlogs } from "./actions/getBlogs";
 import BlogList from "./components/BlogList";
 import FeaturedBlog from "./components/FeaturedBlog";
 import BlogListLoader from "./components/BlogListLoader";
+import FeaturedBlogLoader from "./components/FeaturedBlogLoader";
 
 export default async function Home() {
   return (
     <div className="flex w-full max-w-[1400px] flex-col gap-4 lg:gap-8">
-      <Suspense fallback="Loading...">
+      <Suspense fallback={<FeaturedBlogLoader />}>
         <FeaturedBlog />
       </Suspense>
       <Suspense fallback={<BlogListLoader />}>
