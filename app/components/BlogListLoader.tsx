97224dc8c465a4d0e5b0abb382b1ec21d22db463
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-10 23:14:38 +0530

    minor ui changes

--- app/components/BlogListLoader.tsx
+++ app/components/BlogListLoader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BlogListLoader = () => {
   return (
-    <div className="flex w-full animate-pulse flex-col gap-4 lg:gap-8">
+    <div className="flex w-full max-w-[1000px] animate-pulse flex-col gap-4 lg:gap-8">
       <div className="flex w-full gap-4 p-5">
         <div className="aspect-square w-1/4 max-w-[200px] flex-shrink-0 bg-gray-300"></div>
         <div className="flex w-full flex-col justify-center gap-2">
