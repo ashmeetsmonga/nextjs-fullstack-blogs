9ecd0d39153fd14b3f9fc364262f59525b87e29e
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-08-10 17:52:27 +0530

    user profile details added in page

--- app/actions/getUserDetails.ts
+++ app/actions/getUserDetails.ts
@@ -3,8 +3,7 @@ import prisma from "@/app/libs/prisma";
 export const getUserDetails = async (id: string) => {
   const user = await prisma.user.findUnique({
     where: { id },
-    select: { bio: true },
+    select: { bio: true, name: true, email: true },
   });
-
   return user;
 };
--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,10 +1,38 @@
 import { getUserDetails } from "@/app/actions/getUserDetails";
 import React from "react";
 
-const ProfilePage = ({ params }: { params: { id: string } }) => {
-  const userDetails = getUserDetails(params.id);
+const ProfilePage = async ({ params }: { params: { id: string } }) => {
+  const userDetails = await getUserDetails(params.id);
 
-  return <div>ProfilePage</div>;
+  return (
+    <div className="jus flex w-full flex-col items-center">
+      <div className="flex w-full max-w-[1000px] items-center">
+        <div className="flex w-1/3 shrink-0 justify-center">
+          <div className="rounded-full bg-black px-7 py-8 text-4xl text-white lg:px-12 lg:py-16 lg:text-9xl">
+            {userDetails?.name!.split(" ").map((word) => word[0])}
+          </div>
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <div className="flex w-full flex-col">
+            <h1 className="text-3xl font-semibold leading-snug lg:text-7xl">
+              {userDetails?.name}
+            </h1>
+            <p className="-mt-2 text-sm text-gray-400 lg:text-lg">
+              {userDetails?.email}
+            </p>
+          </div>
+          <p className="hidden text-base font-light lg:block">
+            {userDetails?.bio?.substring(0, 450) +
+              (userDetails?.bio?.length! > 450 ? "..." : "")}
+          </p>
+          <p className="text-xs font-light lg:hidden">
+            {userDetails?.bio?.substring(0, 150) +
+              (userDetails?.bio?.length! > 150 ? "..." : "")}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
